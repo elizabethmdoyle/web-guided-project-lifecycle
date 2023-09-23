5e001f79fdb3907c35e9bf7f1d4ddb32434b5a5d
Elizabeth Doyle <myoddjourney@gmail.com>
2023-09-23 12:15:40 -0400

    working on guided project

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,34 @@
 import React from 'react';
 import axios from 'axios';
+
+const fetchDogs = (breed) => {
+    return axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+        .then( res => {res})
+        .catch( err => {console.log(err)})
+}
+
+class App extends React.Component {
+    constructor() {
+        console.log(`constructor run`)
+        super();
+        this.state = {
+            doggos: [],
+            breed: 'husky' 
+        }
+        console.log(fetchDogs('husky'))
+    }
+
+
+
+    render() {
+        console.log(`render function run`)
+        return (
+              
+                <h1>App</h1>
+                   
+        )
+    
+    }
+}
+
+export default App
